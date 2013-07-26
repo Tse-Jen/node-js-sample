da2f2089d89b6920262a5243c38aa19e4140580e
Tse-Jen Chen <tsejen.chen@gmail.com>
2013-07-26 03:19:03 +0000

    edited web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var buf = new Buffer(fs.readFileSync(index.html));
+var str = buf.toString('utf8',0,buf.length);
+
+response.send(str);
 });
 
 var port = process.env.PORT || 5000;
